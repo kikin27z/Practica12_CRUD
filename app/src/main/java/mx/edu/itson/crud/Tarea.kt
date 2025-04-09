0a3745d8ce60bf4b8eca7dd584187d43f87888df
kikin27z <karimfv04@gmail.com>
2025-04-08 17:05:05 -0700

    Practica avanzada
    Falta que funcione

---
+++ app/src/main/java/mx/edu/itson/crud/Tarea.kt
@@ -0,0 +1,12 @@
+package mx.edu.itson.crud
+
+data class Tarea(var id: String = "",
+    var titulo: String = "",
+    var descripcion: String =""){
+    fun toMap(): Map<String,String>{
+        return mapOf(
+            "titulo" to titulo,
+            "descripcion" to descripcion
+        )
+    }
+}
